2b6736c4312b5b8387e69f6a095a45382fd5cc8b
admin <admin@ADMIN-PC>
2020-06-08 09:49:30 +0700

    wdwdwd

---
+++ BigSchool/viewModels/CourseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class CourseViewModel
+    {
+        private object p;
+
+        public CourseViewModel(object p)
+        {
+            this.p = p;
+        }
+
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+        //hoi loi cho nay
+    }
+}
